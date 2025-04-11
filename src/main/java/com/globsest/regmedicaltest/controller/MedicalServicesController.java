f94a0e49cba914d432b2ad961c76aa757a9e2666
Globsest <globs.noone@mail.ru>
2025-04-11 19:27:39 +0300

    1. Теперь поля формы подгружаются с сервака(надеюсь)

--- src/main/java/com/globsest/regmedicaltest/controller/MedicalServicesController.java
+++ src/main/java/com/globsest/regmedicaltest/controller/MedicalServicesController.java
@@ -2,16 +2,17 @@ package com.globsest.regmedicaltest.controller;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.globsest.regmedicaltest.UserDetailsImpl;
 import com.globsest.regmedicaltest.dto.FormDataDto;
 import com.globsest.regmedicaltest.entity.MedicalServices;
 import com.globsest.regmedicaltest.entity.ServiceForm;
-import com.globsest.regmedicaltest.entity.User;
-import com.globsest.regmedicaltest.repository.MedicalServicesRepository;
+
+import com.globsest.regmedicaltest.repository.ServiceFormRepository;
 import com.globsest.regmedicaltest.service.FormService;
 import com.globsest.regmedicaltest.service.MedicalServiceService;
 import jakarta.validation.Valid;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,28 +29,29 @@ public class MedicalServicesController {
 
     private final MedicalServiceService medicalServiceService;
     private final FormService formService;
+    private final ServiceFormRepository serviceFormRepository;
+    private final ObjectMapper objectMapper;
 
-    @GetMapping
+    @GetMapping("/main")
     ResponseEntity<List<MedicalServices>> getAllMedicalServices() {
         return ResponseEntity.ok(medicalServiceService.getAllMedicalServices());
     }
 
 
     @PostMapping("/submit")
-    ResponseEntity<Long> submitForm(
-            @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @Valid @RequestBody FormDataDto formDataDto) throws JsonProcessingException {
+    ResponseEntity<Long> submitForm(@AuthenticationPrincipal UserDetailsImpl userDetails, @Valid @RequestBody FormDataDto formDataDto) throws JsonProcessingException {
 
         if (userDetails == null) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not authenticated");
         }
 
-        Long recordId = formService.saveForm(
-                userDetails.getId(),
-                formDataDto.getServiceId(),
-                formDataDto.getFormFields()
-        );
+        Long recordId = formService.saveForm(userDetails.getId(), formDataDto.getServiceId(), formDataDto.getFormFields());
 
         return ResponseEntity.ok(recordId);
+    }
+
+    @GetMapping("/{serviceId}/form")
+    ResponseEntity<ServiceForm> getServiceByMedicalService(@PathVariable String serviceId) {
+        return ResponseEntity.ok(medicalServiceService.getServiceByMedicalService(Long.valueOf(serviceId)));
     }
 }
